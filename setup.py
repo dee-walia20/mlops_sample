ee252015db086bbb7d92bab49b1c7bf89d67d186
dee-walia20 <dee.walia20@gmail.com>
2021-04-03 21:36:38 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src', version="0.0.1",
+    description="It's a MLOPS package with wine dataset",
+    author="dee.walia20", 
+    packages = find_packages(),
+    license='MIT'
+)
